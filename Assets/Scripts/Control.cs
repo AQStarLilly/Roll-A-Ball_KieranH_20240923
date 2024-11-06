3c57e8c65c4dba7ad1b49a491b43582838db2e18
AQStarLilly <kieranjhislop@gmail.com>
2024-11-05 23:24:28 -0400

    Level Updates
    You can now go to the next level after you beat the one your on instead of just resetting. Reset will now send you to the first level instead of just resetting the level your on. Extra quick links were added in the menus as well

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -7,8 +7,33 @@ public class Control : MonoBehaviour
 {
     public void ResetTheGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(1);
 
+    }
+
+    public void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if(nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.Log("No more levels to load!");
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
 
