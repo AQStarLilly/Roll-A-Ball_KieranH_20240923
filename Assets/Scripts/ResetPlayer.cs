acbad347a323d9f09d0fe0e2f311be27362e50fc
Kieran Hislop <kieranjhislop@gmail.com>
2024-11-13 09:38:35 -0400

    Added death plane
    Players will now respawn at the start if they fall off the map

---
+++ Assets/Scripts/ResetPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlayer : MonoBehaviour
+{
+    public Transform spawnPoint;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = spawnPoint.position;
+    }
+}
